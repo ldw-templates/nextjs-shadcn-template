b3e2a616ced44385432e1515377c3031947b8c70
Alan Yuan <a@ldwid.com>
2025-08-21 16:17:21 +0800

    feat: move layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,19 +2,15 @@ import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import { ThemeProvider } from "@/components/theme-provider";
-import "@/app/globals.css";
-export default async function LocaleLayout({
+import "./globals.css";
+export default async function Layout({
   children,
   params,
 }: {
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 }) {
-  // Ensure that the incoming `locale` is valid
   const { locale } = await params;
-  if (!hasLocale(routing.locales, locale)) {
-    notFound();
-  }
 
   return (
     <html lang={locale} suppressHydrationWarning>
